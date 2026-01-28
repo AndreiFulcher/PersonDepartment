fcdfe386a35562e5db80c00e4cabd7601017d326
AndreiFulcher <andreii.fulcher@gmail.com>
2026-01-28 20:33:00 -0300

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/controllers/PersonController.java
+++ src/main/java/com/devsuperior/aula/controllers/PersonController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.aula.controllers;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,16 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @PostMapping // Serve para mapear requisições HTTP POST para este método
+   // @PostMapping // Serve para mapear requisições HTTP POST para este método
     public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) { // Serve para indicar que o corpo da requisição será convertido em um objeto ProductDTO
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto); // Retorna uma resposta HTTP 201 Created com o URI do novo recurso no cabeçalho Location
+    }
+
+    @PostMapping // Serve para mapear requisições HTTP POST para este método
+    public ResponseEntity<PersonDTO> insert(@RequestBody PersonDTO dto) { // Serve para indicar que o corpo da requisição será convertido em um objeto ProductDTO
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
